80d1e1b7fb2551a300711ce793cb5dbc88eaa4f0
GRISBIE <sarthaksharma@ocadu.ca>
2025-10-01 18:03:39 -0400

    FwefWEF

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
-function setup() 
-{
-	createCanvas(400, 400);
+let cowGif;
+
+function preload() {
+    cowGif = loadImage('assets/polishcow.gif');
 }
 
-function draw()
-{
+function setup() {
+    createCanvas(400, 400);
+}
 
+function draw() {
+    background(220, 200, 200);
+    image(cowGif, 0, 0);
 }
