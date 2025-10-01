6efbd7b986110e308c4edb76de123814188a3bb7
GRISBIE <sarthaksharma@ocadu.ca>
2025-10-01 18:05:19 -0400

    THIS?

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 let cowGif;
 
+
 function preload() {
-    cowGif = loadImage('assets/polishcow.gif');
+  cowGif = loadImage('/assets/polishcow.gif');
 }
 
 function setup() {
-    createCanvas(400, 400);
-}
-
+  createCanvas(800, 600);
+}	
 function draw() {
-    background(220, 200, 200);
-    image(cowGif, 0, 0);
+  background(220);
+  image(cowGif, 0, 0);
 }
